f0a5907f8be3663143beadcf0c5123f5ab3f7a2a
tareq0mar <95981937+tareq0mar@users.noreply.github.com>
2026-06-25 16:04:28 +0300

    Update App.jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
 import WebSecurityGuide from './web_security_study_guide.jsx'
-import os from './os_study_guide.jsx'
+import OSStudyGuide from './os_study_guide.jsx'
 
 function App() {
   return (
     <div>
       <WebSecurityGuide />
-      <os />
+      <OSStudyGuide />
     </div>
   )
 }
